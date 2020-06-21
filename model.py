ea0caed5aa77c088daaf3377e603c82df36de830
Daniel Zou <zoudl2000@gmail.com>
2020-06-20 22:35:41 -0500

    implemented train_batch

--- model.py
+++ model.py
@@ -5,6 +5,24 @@ import tensorflow.keras.layers as layers
 class ChatbotModel:
     """Seq2Seq model using GRU cells.
     
+    Format for hyperparameters:
+    hparams = {
+        'embedding_size': x,
+        'units': x,
+        'num_layers': x,
+        'keep_prob': x,
+        'learn_rate': x
+    }
+    
+    Format for vocabulary:
+    vocab_int = {
+        '<PAD>': 0,
+        '<SOS>': 1,
+        '<EOS>': 2,
+        '<EX>': 3,
+        ...
+    }
+    
     References:
     Tensorflow 2 tutorial: "Neural machine translation with attention"
     SuperDataScience course: "Deep Learning and NLP A-Z: How to Create a Chatbot"
@@ -13,6 +31,7 @@ class ChatbotModel:
 
     def __init__(self, hparams, vocab_int, filename):
         super(ChatbotModel, self).__init__()
+        self.hparams = hparams
         self.encoder = Encoder(len(vocab_int),
                                hparams['embedding_size'],
                                hparams['units'],
@@ -23,42 +42,80 @@ class ChatbotModel:
                                hparams['units'],
                                hparams['num_layers'],
                                dropout=hparams['keep_prob'])
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=hparams['learning_rate'])
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=hparams['learn_rate'])
         self.vocab_int = vocab_int
         self.filename = filename
         self.loss_obj = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True,
                                                                       reduction='none')
-        self.checkpoint = tf.train.Checkpoint(encoder=encoder, encoder=encoder, decoder=decoder)
+        self.checkpoint = tf.train.Checkpoint(encoder=self.encoder,
+                                              decoder=self.decoder,
+                                              optimizer=self.optimizer)
 
 
-    def train(self, inputs, targets, epochs, batch_size):
-        padded_batch_inputs = tf.keras.preprocessing.sequence.pad_sequences(batch_inputs, padding='post')
-        pass
+    # def train(self, inputs, targets, epochs, batch_size):
+    #     assert len(inputs) == len(targets), 'len(inputs) does not equal len(targets)'
+    #     num_batches = len(inputs)
+    #     for epoch in range(epochs):
+    #         start = time.time()
+    #         hidden_state = tf.zeros((batch_size, self.encoder.units))
+    #         total_loss = 0
 
 
-    def validate(self, inputs, targets):
-        pass
+    def train_batch(self, batch_inputs, batch_targets):
+        """ Used for mini-batch training.
+        batch_inp and batch_targ should be zero-padded.
+        """
+        assert len(batch_inputs) == len(batch_targets), 'len(batch_inp) does not equal len(batch_targ)'
+        batch_size = batch_inputs.shape[0]
+        loss = 0
+        with tf.GradientTape() as tape:
+            init_state = tf.zeros((batch_size, self.encoder.units))
+            enc_outputs, hidden_state = self.encoder(batch_inputs, init_state)
+            dec_inputs = tf.expand_dims([self.vocab_int['<SOS>']] * batch_size, 1)
+            
+            # teacher forcing: feeding the target as the next input
+            for t in range(1, batch_targets.shape[1]):
+                predict_words, _, _ = self.decoder(dec_inputs, hidden_state, enc_outputs)
+                target_words = batch_targets[:, t]
+                loss += self._loss_fn(target_words, predict_words)
+                dec_inputs = tf.expand_dims(target_words, 1)
+                
+        batch_loss = loss / batch_size
+        variables = self.encoder.trainable_variables + self.decoder.trainable_variables
+        gradients = tape.gradient(loss, variables)
+        self.optimizer.apply_gradients(zip(gradients, variables))
+        return batch_loss
+
+
+    def validate_batch(self, batch_inputs, batch_targets):
+        """ Validation during the training cycle. Loss is not backproped.
+        batch_inp and batch_targ should be zero-padded.
+        """
+        assert len(batch_inputs) == len(batch_targets), 'len(batch_inp) does not equal len(batch_targ)'
+        batch_size = batch_inputs.shape[0]
+        loss = 0
+        
+        
 
 
     def predict(self, inputs, max_output_len):
         pass
 
 
-    def _load_model(self):
-        self.save_dir
+    def save(self):
+        self.checkpoint.save(self.filename)
+
+
+    def load(self):
+        self.filename
         pass
 
 
-    def _save_model(self):
-        self.save_dir
-        pass
-    
-    
-    def _loss_fn(targets, predictions):
+    def _loss_fn(self, targets, predictions):
         loss = self.loss_obj(targets, predictions)
-        # mask = tf.math.logical_not(tf.math.equal(targets, 0))
-        # mask = tf.cast(mask, dtype=loss.dtype)
-        # loss *= mask
+        mask = tf.math.logical_not(tf.math.equal(targets, 0))
+        mask = tf.cast(mask, dtype=loss.dtype)
+        loss *= mask
         return tf.math.reduce_mean(loss)
 
 
@@ -67,6 +124,7 @@ class Encoder(tf.keras.Model):
     def __init__(self, vocab_size, embedding_dim, units, num_layers, dropout=0.):
         super(Encoder, self).__init__()
         self.units = units
+        self.embedding_dim = embedding_dim
         self.embedding = layers.Embedding(vocab_size, embedding_dim, mask_zero=True)
         gru_cells = [layers.GRUCell(units,
                                     recurrent_initializer='glorot_uniform',
@@ -77,10 +135,11 @@ class Encoder(tf.keras.Model):
                                                    return_state=True))
 
 
-    def call(self, x, state):
-        x = self.embedding(x)
-        y, state = self.gru(x, initial_state=state)
-        return y, state
+    def call(self, inputs, state):
+        # inputs are in mini-batches
+        inputs = self.embedding(inputs)
+        outputs, state = self.gru(inputs, initial_state=state)
+        return outputs, state
 
 
 class Decoder(tf.keras.Model):
@@ -88,8 +147,9 @@ class Decoder(tf.keras.Model):
     def __init__(self, vocab_size, embedding_dim, units, num_layers, dropout=0.):
         super(Decoder, self).__init__()
         self.units = units
-        self.attention = BahdanauAttention(units)
+        self.embedding_dim = embedding_dim
         self.embedding = layers.Embedding(vocab_size, embedding_dim)
+        self.attention = BahdanauAttention(units)
         gru_cells = [layers.GRUCell(units,
                                     recurrent_initializer='glorot_uniform',
                                     dropout=dropout)
@@ -97,22 +157,22 @@ class Decoder(tf.keras.Model):
         self.gru = layers.Bidirectional(layers.RNN(gru_cells,
                                                    return_sequences=True,
                                                    return_state=True))
-        self.fc = tf.keras.layers.Dense(vocab_size)
+        self.predictor = tf.keras.layers.Dense(vocab_size)
 
 
-    def call(self, x, state, enc_outputs):
-        # enc_output shape == (batch_size, max_len, state_size)
+    def call(self, inputs, state, enc_outputs):
+        # enc_outputs shape == (batch_size, max_len, state_size)
         context_vec, _ = self.attention(state, enc_outputs)
-        x = self.embedding(x)
-        x = tf.concat([tf.expand_dims(context_vec, 1), x], axis=-1)
+        inputs = self.embedding(inputs)
+        inputs = tf.concat([tf.expand_dims(context_vec, 1), inputs], axis=-1)
         # x.shape == (batch_size, 1, embedding_dim + state_size)
 
-        y, state = self.gru(x)
-        y = tf.reshape(y, (-1, y.shape[2]))
-        # y.shape == (batch_size, vocab)
-
-        output = self.fc(y)
-        return output, state
+        outputs, state = self.gru(inputs)
+        outputs = tf.reshape(outputs, (-1, outputs.shape[2]))
+        predictions = self.predictor(outputs)
+        # predictions.shape == (batch_size, vocab_size)
+        
+        return predictions, state
 
 
 class BahdanauAttention(layers.Layer):
--- train.py
+++ train.py
@@ -1,9 +1,62 @@
 from sklearn.model_selection import train_test_split
 import numpy as np
 import time
+import utils
+
+
+############ PREPROCESSING DATA ############
+
+with open('cmdc/movie_lines.txt', encoding='utf-8', errors='ignore') as f:
+    lines = f.read().split('\n')
+with open('cmdc/movie_conversations.txt', encoding='utf-8', errors='ignore') as f:
+    conversations = f.read().split('\n')
+
+# Extract file data
+id_to_line = {}
+for line in lines:
+    line = line.split(" +++$+++ ")
+    if len(line) == 5:
+        id_to_line[line[0]] = line[-1]
+
+conversation_ids = []
+for conv in conversations[:-1]:
+    conv = conv.split(" +++$+++ ")
+    if len(conv) == 4:
+        conv = conv[-1][1:-1].replace("'", "").replace(" ", "")
+        conversation_ids.append(conv.split(","))
+
+questions = []
+answers = []
+for cid in conversation_ids:
+    for i in range(len(cid) - 1):
+        questions.append(id_to_line[cid[i]])
+        answers.append(id_to_line[cid[i + 1]])
+
+# Clean non-alphanumeric characters
+clean_questions = []
+for q in questions:
+    clean_questions.append(utils.clean_text(q))
+
+clean_answers = []
+for a in answers:
+    clean_answers.append(utils.clean_text(a))
+
+vocab_int = {
+    '<PAD>': 0,
+    '<SOS>': 1,
+    '<EOS>': 2,
+    '<EX>': 3
+}
+
+tf.keras.preprocessing.text.Tokenizer
 
 EPOCHS = 10
 BATCH_SIZE = 32
-tf.keras.preprocessing.text.Tokenizer
 
-# vocab_int = ['<PAD>', '<SOS>', '<EOS>', '<OUT>', ...]
+
+for (batch_i, (batch_input, batch_target)) in enumerate():
+    pad_batch_inp = tf.keras.preprocessing.sequence.pad_sequences(batch_inp, padding='post')
+    pad_batch_targ = tf.keras.preprocessing.sequence.pad_sequences(batch_targ, padding='post')
+    break
+
+# k-fold cross validation
